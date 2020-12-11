afbee631f32d6666acc42dcab4b3a972d4152eaf
quentintrouve <trouve.quentin@gmail.com>
2020-12-11 18:54:29 +0100

    add provider and connect method

--- src/Compteur/Compteur.js
+++ src/Compteur/Compteur.js
@@ -1,42 +1,41 @@
-
-
 import React, { Component } from 'react'
 import './Compteur.css';
 import Resultat from '../Composants/Resultat/Resultat.js';
 import IncrBtn from '../Composants/IncrBtn/IncrBtn.js';
+import { connect } from 'react-redux';
 
 class Compteur extends Component {
 
     state = {
-        compteur : 0
+        compteur: 0
     }
 
-     calcul = (action) => {
+    calcul = (action) => {
 
-        if(action === "plus1"){
-            this.setState( ( statePrec ) => { return { compteur: statePrec.compteur + 1 } } )
-        } 
-        if(action === "moins1"){
-            this.setState( ( statePrec ) => { return { compteur: statePrec.compteur - 1 } } )
+        if (action === "plus1") {
+            this.setState((statePrec) => { return { compteur: statePrec.compteur + 1 } })
         }
-    
-        
+        if (action === "moins1") {
+            this.setState((statePrec) => { return { compteur: statePrec.compteur - 1 } })
+        }
+
+
     }
-    
+
 
     render() {
         return (
             <div className="cont">
-            <div className="blocConmpt">
-                <Resultat valeur={this.state.compteur} />
+                <div className="blocConmpt">
+                    <Resultat valeur={this.state.compteur} />
+                </div>
+                <div className="contBtn">
+                    <IncrBtn txt="Incremente" clicked={() => this.calcul("plus1")} />
+                    <IncrBtn txt="Decremente" clicked={() => this.calcul("moins1")} />
+                </div>
             </div>
-            <div className="contBtn">
-                <IncrBtn txt="Incremente" clicked={() => this.calcul("plus1")} />
-                <IncrBtn txt="Decremente" clicked={() => this.calcul("moins1")} />
-            </div>
-        </div>
         )
     }
 }
 
-export default Compteur;
+export default connect()(Compteur);
